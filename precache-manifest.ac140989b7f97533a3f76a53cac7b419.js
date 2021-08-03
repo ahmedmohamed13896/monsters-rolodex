fb608912eed671e1e00133e50ebe5a8855b25e95
ahmed mohamed <ahmedmohamed77765@hotmail.com>
2021-08-03 23:34:23 +0200

    Updates

---
+++ precache-manifest.ac140989b7f97533a3f76a53cac7b419.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107b32a3ced36428e06a4125cd036189",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec6732dd25240e472b74",
+    "url": "/monsters-rolodex/static/css/main.85227b22.chunk.css"
+  },
+  {
+    "revision": "e3dc37ae85111b460d12",
+    "url": "/monsters-rolodex/static/js/2.071da586.chunk.js"
+  },
+  {
+    "revision": "ec6732dd25240e472b74",
+    "url": "/monsters-rolodex/static/js/main.7869bda1.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
